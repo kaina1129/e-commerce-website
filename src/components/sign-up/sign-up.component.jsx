b19e52f8fa6259e7d75655f4b520570c746d2efb
kaina1129 <zhazhe9494@gmail.com>
2020-10-23 20:18:44 -0400

    finish sign in handle submit method

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -21,6 +21,7 @@ class SignUp extends React.Component {
 
 	handleSubmit = async (event) => {
 		event.preventDefault();
+		
 
 		const { displayName, email, password, confirmPassword } = this.state;
 
